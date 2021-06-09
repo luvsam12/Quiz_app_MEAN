3c098a204d82da0f3a86d875ed51e10f0d09242c
luvsam <i_naman.s@olcademy.in>
2021-06-10 00:06:59 +0530

    added data to carousel, font-styles, header-fixed

--- frontend/quiz-app/src/app/aboutme/aboutme.component.ts
+++ frontend/quiz-app/src/app/aboutme/aboutme.component.ts
@@ -13,7 +13,37 @@ import { Label } from 'ng2-charts';
 export class AboutmeComponent implements OnInit {
   check:boolean = true;
   arr: Array<number> = [1,2,3]
-
+  number_data = [
+    {
+      name: "lines of code.",
+      data: 3049
+    },
+    {
+      name: "pixels rendered",
+      data: 154029
+    },
+    {
+      name: "projects",
+      data: 0
+    }
+  ]
+  quote_data = [
+    {
+      msg: "  Many of life's failures are people who did not realize how close they were to success when they gave up.  ",
+      person: "https://i.guim.co.uk/img/media/b933fd4eb7788b737b0b304ec777447d8ac6ae93/0_183_3200_1918/master/3200.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=bfd66629efcac9d0fef15b5cb7e33396",
+      name: "Thomas A. Edison"
+    },
+    {
+      msg: "If you're changing the world, you're working on important things. You're excited to get up in the morning.",
+      person: "https://akshaysaini.in/img/quotes/larry-page.jpg",
+      name: "Larry Page"
+    },
+    {
+      msg: "Because the people who are crazy enough to think they can change the world are the ones who do.",
+      person: "https://akshaysaini.in/img/quotes/steve-jobs.jpg",
+      name: "Steve Jobs"
+    }
+  ]
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 
   //core skills
@@ -40,6 +70,42 @@ export class AboutmeComponent implements OnInit {
 
   ngOnInit(): void {
     console.log(this.images)
+    this.numberWithSpaces("lines")
+    this.numberWithSpaces("pixels")
+    this.numberWithSpaces("proj")
+  }
+
+  numberDisplay(count){
+    return count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+  }
+
+  numberWithSpaces(name) {
+    let set = setInterval(()=>{
+      if(name == "lines"){
+        if(this.number_data[0].data < 3749){
+          this.number_data[0].data++;
+        }
+        else{
+          clearInterval(set);
+        }
+      }
+      else if(name == "pixels"){
+        if(this.number_data[1].data < 154629){
+          this.number_data[1].data++;
+        }
+        else{
+          clearInterval(set);
+        }
+      }
+      else if(name == "proj"){
+        if(this.number_data[2].data < 5){
+          this.number_data[2].data++;
+        }
+        else{
+          clearInterval(set);
+        }
+      }
+    },10)
   }
 
   download(){
